f58686013f687f3ac685df1fa4b5dfdf7022482a
Christopher Wiley <wiley.christopher@gmail.com>
2012-03-26 21:47:55 -0500

    Changed diff filter to a more OO style

--- src/difffilter.py
+++ src/difffilter.py
@@ -1,27 +1,34 @@
-#!/usr/bin/python
+import os
 
-import sys
-if len(sys.argv) != 2:
-    sys.stderr.write('FAIL FAIL FAIL need a filter type')
-    exit(-1)
+class DiffFilter:
+    def __init__(self, extension):
+        self.fileEnding = '.' + extension + os.linesep
 
-lineending="\n"
-filefilter='.'+sys.argv[1] + lineending
+    def filter(inpath, outpath):
+        oneago=''
+        valid=False
+        sentinel = (
+                '===================================' +
+                '===================================' +
+                + os.linesep)
+        try:
+            inf = open(inpath, 'r')
+            outf = open(outpath, 'w')
 
-oneago=''
-valid=False
-sentinel='===================================================================' + lineending
-
-for currline in sys.stdin:
-    if currline == sentinel and oneago.startswith('Index: '):
-        # we're at the start of a new diff section
-        if oneago.endswith(filefilter):
-            # oneago is the index line
-            valid=True
-        else:
-            valid=False
-
-    if valid:
-        sys.stdout.write(oneago)
-
-    oneago=currline
+            for currline in inf:
+                if currline == sentinel and oneago.startswith('Index: '):
+                    # we're at the start of a new diff section
+                    if oneago.endswith(self.fileEnding):
+                        # oneago is the index line
+                        valid=True
+                    else:
+                        valid=False
+                if valid:
+                    outf.write(oneago)
+                    outf.write('\n')
+                oneago=currline
+            inf.close()
+            outf.close()
+        except IOError:
+            return False
+        return True
