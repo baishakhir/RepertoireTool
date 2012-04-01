88219964d1f38dc263b8723263a545f83885418a
Christopher Wiley <wiley.christopher@gmail.com>
2012-04-01 09:58:45 -0500

    Just removing some globals, making them instance

--- src/diff_display.py
+++ src/diff_display.py
@@ -3,25 +3,24 @@ import sys
 from PyQt4 import QtCore, QtGui
 from ui.file_display import Ui_Repertoire
 
-inf1 = ""
-inf2 = ""
-
 class RepDisplay(QtGui.QMainWindow):
-	def __init__(self, parent=None):
+    filePath1 = ''
+    filePath2 = ''
+	def __init__(self, parent=None, filePath1, filePath2):
 		QtGui.QWidget.__init__(self, parent)
+        self.filePath1 = filePath1
+        self.filePath2 = filePath2
 		self.ui = Ui_Repertoire()
 		self.ui.setupUi(self)
 		self.display()
 		self.postSetup()
 
 	def display(self):
-		global inf1
-		global inf2
-		self.ui.file_name1.setText(inf1)
-		self.ui.file_name2.setText(inf2)
-		from os.path import isfile	
-		if isfile(inf1):
-			text = open(inf1).read()
+		self.ui.file_name1.setText(self.filePath1)
+		self.ui.file_name2.setText(self.filePath2)
+		from os.path import isfile
+		if isfile(self.filePath1):
+			text = open(self.filePath1).read()
 			textcolor = QtGui.QColor("red")
 			textfont = QtGui.QFont("courier",8)
 			self.ui.file1.setTextColor(textcolor)
@@ -30,9 +29,9 @@ class RepDisplay(QtGui.QMainWindow):
 		textcolor = QtGui.QColor("blue")
 		self.ui.file1.setTextColor(textcolor)
 		self.ui.file1.append("test")
-		
-		if isfile(inf2):
-			text = open(inf2).read()
+
+		if isfile(self.filePath2):
+			text = open(self.filePath2).read()
 			self.ui.file2.setText(text)
 
 	def postSetup(self):
@@ -42,11 +41,14 @@ class RepDisplay(QtGui.QMainWindow):
 		sys.exit()
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print "Usage: diff_display.py <path to file 1> <path to file 2>"
+        return
 	app = QtGui.QApplication(sys.argv)
 	inf1 = sys.argv[1]
 	print inf1
 	inf2 = sys.argv[2]
 	print inf2
-	myapp = RepDisplay()
+	myapp = RepDisplay(parent = None, inf1, inf2)
 	myapp.show()
 	sys.exit(app.exec_())
