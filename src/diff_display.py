3c6ed9f8b674b3d14923c8594e89ce7706113ea1
Christopher Wiley <wiley.christopher@gmail.com>
2012-04-01 11:18:30 -0500

    Refactoring display tools to use output parser
    This makes them shorter and simpler.  Also, fixed mistakes I made when I
    refactored these files the first time.

--- src/diff_display.py
+++ src/diff_display.py
@@ -4,51 +4,49 @@ from PyQt4 import QtCore, QtGui
 from ui.file_display import Ui_Repertoire
 
 class RepDisplay(QtGui.QMainWindow):
-    filePath1 = ''
-    filePath2 = ''
-	def __init__(self, parent=None, filePath1, filePath2):
-		QtGui.QWidget.__init__(self, parent)
+    def __init__(self, filePath1, filePath2, parent=None):
+        QtGui.QWidget.__init__(self, parent)
         self.filePath1 = filePath1
         self.filePath2 = filePath2
-		self.ui = Ui_Repertoire()
-		self.ui.setupUi(self)
-		self.display()
-		self.postSetup()
+        self.ui = Ui_Repertoire()
+        self.ui.setupUi(self)
+        self.display()
+        self.postSetup()
 
-	def display(self):
-		self.ui.file_name1.setText(self.filePath1)
-		self.ui.file_name2.setText(self.filePath2)
-		from os.path import isfile
-		if isfile(self.filePath1):
-			text = open(self.filePath1).read()
-			textcolor = QtGui.QColor("red")
-			textfont = QtGui.QFont("courier",8)
-			self.ui.file1.setTextColor(textcolor)
-			self.ui.file1.setFont(textfont)
-			self.ui.file1.setText(text)
-		textcolor = QtGui.QColor("blue")
-		self.ui.file1.setTextColor(textcolor)
-		self.ui.file1.append("test")
+    def display(self):
+        self.ui.file_name1.setText(self.filePath1)
+        self.ui.file_name2.setText(self.filePath2)
+        from os.path import isfile
+        if isfile(self.filePath1):
+            text = open(self.filePath1).read()
+            textcolor = QtGui.QColor("red")
+            textfont = QtGui.QFont("courier",8)
+            self.ui.file1.setTextColor(textcolor)
+            self.ui.file1.setFont(textfont)
+            self.ui.file1.setText(text)
+        textcolor = QtGui.QColor("blue")
+        self.ui.file1.setTextColor(textcolor)
+        self.ui.file1.append("test")
 
-		if isfile(self.filePath2):
-			text = open(self.filePath2).read()
-			self.ui.file2.setText(text)
+        if isfile(self.filePath2):
+            text = open(self.filePath2).read()
+            self.ui.file2.setText(text)
 
-	def postSetup(self):
-		self.ui.pushButton.clicked.connect(self.closeDisplay)
+    def postSetup(self):
+        self.ui.pushButton.clicked.connect(self.closeDisplay)
 
-	def closeDisplay(self):
-		sys.exit()
+    def closeDisplay(self):
+        sys.exit()
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print "Usage: diff_display.py <path to file 1> <path to file 2>"
-        return
-	app = QtGui.QApplication(sys.argv)
-	inf1 = sys.argv[1]
-	print inf1
-	inf2 = sys.argv[2]
-	print inf2
-	myapp = RepDisplay(parent = None, inf1, inf2)
-	myapp.show()
-	sys.exit(app.exec_())
+        sys.exit()
+    app = QtGui.QApplication(sys.argv)
+    inf1 = sys.argv[1]
+    print inf1
+    inf2 = sys.argv[2]
+    print inf2
+    myapp = RepDisplay(inf1, inf2)
+    myapp.show()
+    sys.exit(app.exec_())
--- src/rep_display.py
+++ src/rep_display.py
@@ -2,132 +2,98 @@
 import sys
 import os
 from PyQt4 import QtCore, QtGui
-from PyQt4.QtGui import QApplication, QTreeWidget, QTreeWidgetItem, QMenu 
-     
-from ui.rep_output import Ui_RepOutput
+from PyQt4.QtGui import QApplication, QTreeWidget, QTreeWidgetItem, QMenu
 
+from ui.rep_output import Ui_RepOutput
+from output_parser import RepertoireOutput
 import config
 
-
-inf = ""
-file_list = []
-
 class RepOutput(QtGui.QMainWindow):
-	def __init__(self, parent=None):
-		QtGui.QWidget.__init__(self, parent)
-		self.ui = Ui_RepOutput()
-		self.ui.setupUi(self)
+    def __init__(self, rep_out_path, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.repOutputPath = rep_out_path
+        self.fileList = []
+        self.ui = Ui_RepOutput()
+        self.ui.setupUi(self)
 
-		self.postSetup()
-		self.process_clone()
+        self.postSetup()
+        self.process_clone()
 
-	def postSetup(self):
-		self.ui.fileList.setColumnWidth(0,100)
-		self.ui.fileList.setColumnWidth(1,600)
+    def postSetup(self):
+        self.ui.fileList.setColumnWidth(0,100)
+        self.ui.fileList.setColumnWidth(1,600)
 
-		self.ui.cloneList.setColumnWidth(0,200)
-		self.ui.cloneList.setColumnWidth(1,400)
-		self.ui.cloneList.itemClicked.connect(self.onClick)
+        self.ui.cloneList.setColumnWidth(0,200)
+        self.ui.cloneList.setColumnWidth(1,400)
+        self.ui.cloneList.itemClicked.connect(self.onClick)
 
-	def onClick(self):
-		global file_list
-		item = self.ui.cloneList.currentItem()
-		clone = str(item.text(1))
-#		print clone
-		cl1,cl2 = clone.split("\t")
-		indx1,cl1 = cl1.split(".")
-		indx2,cl2 = cl2.split(".")
-		fname1 = file_list[int(indx1)-1]
-		fname2 = file_list[int(indx2)-1]
-		clone1 = fname1 + "." + cl1
-		clone2 = fname2 + "." + cl2
-		args = "./diff_display.py " + fname1 + " " + fname2
-		print args	
-		os.system(args)
-#		print clone1
-#		print clone2
+    def onClick(self):
+        item = self.ui.cloneList.currentItem()
+        clone = str(item.text(1))
+#        print clone
+        cl1,cl2 = clone.split("\t")
+        indx1,cl1 = cl1.split(".")
+        indx2,cl2 = cl2.split(".")
+        fname1 = self.fileList[int(indx1)-1]
+        fname2 = self.fileList[int(indx2)-1]
+        clone1 = fname1 + "." + cl1
+        clone2 = fname2 + "." + cl2
+        args = "./diff_display.py " + fname1 + " " + fname2
+        print args
+        os.system(args)
+#        print clone1
+#        print clone2
 
-	def contextMenuEvent(self,event):
-		if event.reason() == event.Mouse:
-		  pos = event.globalPos() 
-		  item = self.ui.cloneList.itemAt(pos)	
-						
-		  if pos is not None:
-				menu = QMenu(self)
-				menu.addAction(item.text(0))
-				menu.popup(Qcpos)
+    def contextMenuEvent(self,event):
+        if event.reason() == event.Mouse:
+          pos = event.globalPos()
+          item = self.ui.cloneList.itemAt(pos)
 
-		  event.accept() 
+          if pos is not None:
+                menu = QMenu(self)
+                menu.addAction(item.text(0))
+                menu.popup(Qcpos)
 
-	def process_clone(self):
-		
-		global inf
-		global file_list 
-		fileId = open(inf,"r")
+          event.accept()
 
-		orig_line_number = 0
-		source_file_processing = 0
-		clone_pairs_processing = 0
+    def process_clone(self):
+        rep_output = RepertoireOutput()
+        rep_output.loadFromFile(self.repOutputPath)
 
-		for line in fileId:
+        for index, fileName in rep_output.getFileIter():
+            if config.DEBUG == 2:
+                print "fileName =" + fileName
+            self.fileList.append(fileName)
+            listItem = QtGui.QTreeWidgetItem(self.ui.fileList)
+            listItem.setText(0,str(index))
+            listItem.setText(1,fileName)
 
-			orig_line_number=orig_line_number+1
+        for indx, (cl1, cl2) in rep_output.getCloneIter():
+            fidx1, start1, end1 = cl1
+            fidx2, start2, end2 = cl2
+            metric = max(end1 - start1, end2 - start2)
 
-			if line.startswith("source_files {"):
-				source_file_processing = 1
-			elif line.startswith("clone_pairs {"):
-				clone_pairs_processing = 1
-			elif line.startswith("}"):
-				source_file_processing = 0
-				clone_pairs_processing = 0
-			else:
-				if source_file_processing is 1:
-					index,fileName,dummy = line.split("\t")
-#					fileName,extn = os.path.splitext(fileName)
-					if (config.DEBUG == 2):
-						print "fileName =" + fileName
-					file_list.append(fileName)
-					listItem = QtGui.QTreeWidgetItem(self.ui.fileList)
-					listItem.setText(0,index)
-					listItem.setText(1,fileName)
+            if (config.DEBUG == 2):
+                print line
+                print indx + " " + cl1 + " " + cl2
+                print metric
 
-				elif clone_pairs_processing is 1:
-					indx,cl1,cl2,metric = line.split("\t")
-					
-					metric = metric.strip("(")
-					metric = metric.strip(")\n")
-					metric1,metric2 = metric.split(":")
-					if(int(metric1) > int(metric2)):
-						metric = int(metric1)
-					else:
-						metric = int(metric2)
+            if metric:
+                listItem = QtGui.QTreeWidgetItem(self.ui.cloneList)
+                listItem.setText(0, str(indx))
+                listItem.setText(1,"{0}.{1}-{2}\t{3}.{4}-{5}".format(
+                    fidx1, start1, end1, fidx2, start2, end2))
+                listItem.setText(2,str(metric))
 
-
-					if (config.DEBUG == 2):
-						print line
-						print indx + " " + cl1 + " " + cl2
-						print metric
-
-					if(metric):
-						listItem = QtGui.QTreeWidgetItem(self.ui.cloneList)
-						listItem.setText(0,indx)
-						cl = cl1 + "\t" + cl2
-						listItem.setText(1,cl)
-						listItem.setText(2,str(metric))
-					
-				else:
-					pass
-
-		fileId.close()
-
-	
-
-	def closeDisplay(self):
-		sys.exit()
+    def closeDisplay(self):
+        sys.exit()
 
 if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
-	inf = sys.argv[1]
-	myapp = RepOutput()
-	myapp.show()
-	sys.exit(app.exec_())
+    if len(sys.argv) != 2:
+        print "Usage: display_rep.py <path to repertoire output>"
+        sys.exit()
+    app = QtGui.QApplication(sys.argv)
+    inf = sys.argv[1]
+    myapp = RepOutput(inf)
+    myapp.show()
+    sys.exit(app.exec_())
